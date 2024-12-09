e204cce8e5a1b55704d522b6645dec8a216e7e59
opVOID <mannaushack@protonmail.com>
2024-12-09 03:55:51 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import '@solana/wallet-adapter-react-ui/styles.css'
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
-import { GambaPlatformProvider, ReferralProvider, TokenMetaProvider } from 'gamba-react-ui-v2'
+import { GambaPlatformProvider, TokenMetaProvider } from 'gamba-react-ui-v2'
 import { GambaProvider, SendTransactionProvider } from 'gamba-react-v2'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { DEFAULT_POOL, PLATFORM_CREATOR_ADDRESS, PLATFORM_CREATOR_FEE, PLATFORM_JACKPOT_FEE, PLATFORM_REFERRAL_FEE, RPC_ENDPOINT, TOKEN_METADATA, TOKEN_METADATA_FETCHER } from './constants'
+import { DEFAULT_POOL, PLATFORM_CREATOR_ADDRESS, PLATFORM_CREATOR_FEE, PLATFORM_JACKPOT_FEE, RPC_ENDPOINT, TOKEN_METADATA, TOKEN_METADATA_FETCHER } from './constants'
 import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
@@ -42,12 +42,7 @@ function Root() {
                     defaultJackpotFee={PLATFORM_JACKPOT_FEE}
                     defaultPool={DEFAULT_POOL}
                   >
-                    <ReferralProvider
-                      prefix="code"
-                      fee={PLATFORM_REFERRAL_FEE}
-                    >
                       <App />
-                    </ReferralProvider>
                   </GambaPlatformProvider>
                 </GambaProvider>
               </SendTransactionProvider>
